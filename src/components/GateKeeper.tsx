16d8e67ac1b50c73f81a513d48004244ee152997
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-08 15:07:33 +1000

    Add themes

--- src/components/GateKeeper.tsx
+++ src/components/GateKeeper.tsx
@@ -4,78 +4,8 @@ import React from 'react';
 import { Authenticator, Greetings } from 'aws-amplify-react';
 import { UsernameAttributes } from 'aws-amplify-react/lib-esm/Auth/common/types';
 import { I18n } from 'aws-amplify';
-
-I18n.putVocabularies({
-  pt: {
-    // AuthPiece
-    Email: 'Email',
-    'Enter your email': 'Entre com seu email',
-    'Enter your username': 'Nome de usuário',
-
-    // ConfirmSignIn
-    'Confirm Code': 'Confirmar código',
-    'Confirm  Code': 'Confirmar código',
-    'Confirm SMS Code': 'Confirmar código SMS',
-    'Confirm TOPT Code': 'Confirmar código TOPT',
-    'Confirm SOFTWARE_TOKEN_MFA Code': 'Confirmar código SOFTWARE_TOKEN_MFA',
-    Code: 'Código',
-    Confirm: 'Confirmar',
-    'Back to Sign In': 'Voltar',
-
-    // ConfirmSignUp
-    'Confirm Sign Up': 'Confirmar cadastro',
-    Username: 'Nome de usuário',
-    'Confirmation Code': 'Código de confirmação',
-    'Enter your code': 'Entre o código',
-    'Lost your code? ': 'Perdeu o código? ',
-    'Resend Code': 'Enviar código novamente',
-
-    // FederatedSignIn
-    or: 'ou',
-
-    // ForgotPassword
-    'New Password': 'Nova senha',
-    'Reset your password': 'Redefinir a senha',
-    Submit: 'Enviar',
-    'Send Code': 'Enviar código',
-
-    // Greetings
-    Hello: 'Olá',
-
-    // Loading
-    'Loading...': 'Carregando...',
-
-    // RequireNewPassword
-    Change: 'Alterar',
-    'Change Password': 'Alterar senha',
-
-    // SignIn
-    'Sign in to your account': 'Fazer o login',
-    Password: 'Senha',
-    'Enter your password': 'Entre com sua senha',
-    'Forget your password? ': 'Esqueceu sua senha? ',
-    'Forgot your password? ': 'Esqueceu sua senha? ',
-    'Reset password': 'Resetar senha',
-    'Sign In': 'Entrar',
-    'No account? ': 'Novo cliente? ',
-    'Create account': 'Criar conta',
-
-    // SignOut
-    'Sign Out': 'Sair',
-
-    // SignUp
-    'Create Account': 'Criar conta',
-    'Have an account? ': 'Já tem conta? ',
-    'Sign in': 'Entrar',
-
-    // VerifyContact
-    'Phone Number': 'Telefone',
-    'Account recovery requires verified contact information':
-      'A recuperação da conta requer informações verificadas',
-    Verify: 'Verificar',
-    Skip: 'Pular',
-  },
-});
+import { theme3 as theme } from './themes';
+import './translations';
 
 const signUpConfigEn = {
   hiddenDefaults: ['phone_number'],
@@ -113,6 +43,7 @@ export const GateKeeper: React.FC<IGateKeeperProps> = ({ lang = 'pt' }) => {
   return (
     <Authenticator
       hide={[Greetings]}
+      theme={theme}
       signUpConfig={lang === 'pt' ? signUpConfigPt : signUpConfigEn}
       usernameAttributes={UsernameAttributes.EMAIL}
     />
