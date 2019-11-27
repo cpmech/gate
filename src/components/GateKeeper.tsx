44c8c9c5ab8864edddc16f82348bcde12d692f0c
Dorival Pedroso <dorival.pedroso@gmail.com>
2019-11-27 15:55:47 +1000

    Import components locally

--- src/components/GateKeeper.tsx
+++ src/components/GateKeeper.tsx
@@ -6,7 +6,8 @@ import { CognitoHostedUIIdentityProvider } from '@aws-amplify/auth/lib-esm/types
 import { UsernameAttributes } from 'aws-amplify-react/lib-esm/Auth/common/types';
 import { IconFacebookCircle, IconGoogle } from '@cpmech/react-icons';
 import { Pair } from 'rcomps';
-import { PageLoading, PageNoAccess } from 'components';
+import { PageLoading } from './PageLoading';
+import { PageNoAccess } from './PageNoAccess';
 import { GateStore } from './GateStore';
 import { theme3 as theme } from './themes';
 import { stylesGateKeeper as styles } from './styles';
