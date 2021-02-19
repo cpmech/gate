147550a964dca7e4be211cafa28fd8be42bed9bb
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-02-19 15:04:10 +1000

    Add dist script

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
 # React Components for AWS Cognito
 
-TODO
+## Installation
 
+Run:
+
+```bash
+npm install @cpmech/gate
+```
+
+Add the following to your `npm_postinstall.bash` script
+
+
+```bash
+if [[ -d "./node_modules/@cpmech/gate/gate" ]]; then
+    echo ">>> copy gate to src <<<"
+    rm -rf ./src/gate
+    cp -rf ./node_modules/@cpmech/gate/gate ./src/
+fi
+```
---
+++ zscripts/npm_dist.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+OLDVERSION=`jq -r '.version' package.json`
+
+echo
+echo "The current version is: $OLDVERSION"
+
+echo
+echo "What is the new version?"
+read NEWVERSION
+
+echo
+echo "The new version is: $NEWVERSION"
+
+if [ "$NEWVERSION" = "$OLDVERSION" ]; then
+    echo
+    echo "New version must be different than the current version"
+    exit 1
+fi
+
+echo
+echo "Continue? [y/n]"
+read CONTINUE
+
+if [ "$CONTINUE" != "y" ]; then
+    echo
+    echo "Ok, bye"
+fi
+
+change() {
+    file=$1
+    jq '.version = $newVal' --arg newVal $NEWVERSION $file > tmp.$$ && mv -f tmp.$$ $file
+}
+
+# change version
+change package.json
+change package-dist.json
+
+# compile code
+echo
+echo "🔥 Copying lib"
+rm -rf dist
+mkdir -p dist/gate
+rsync -av src/lib/* dist/gate --exclude __stories__ --exclude __tests__
+
+# publish
+echo
+echo "🚀 Publishing"
+cp package-dist.json dist/package.json
+cd dist
+npm publish --access=public
+
+# git tag
+echo
+echo "🏷️ Tagging"
+cd ..
+git add package.json package-dist.json
+git tag v`jq -r '.version' package.json`
+git commit -m 'Publish' && git push && git push --tags
+
+# done
+echo
+echo "😀 DONE"
