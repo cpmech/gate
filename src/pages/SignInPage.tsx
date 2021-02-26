f8a8627d6ab38641c9bb2fed6a21e9cbaefb038b
Dorival Pedroso <dorival.pedroso@gmail.com>
2021-02-26 12:48:33 +1000

    Add earth anim

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,16 +1,54 @@
 /** @jsxImportSource @emotion/react */
-import { IconEarth } from '@cpmech/iricons/IconEarth';
 import { css } from '@emotion/react';
 import { GateSignUpForm, GateStore, LocalGateSignUpForm, LocalGateStore } from '../lib';
 import { gate, isLocal } from '../service';
 
+const Logo: React.FC = () => (
+  <div
+    css={css`
+      width: 150px;
+      height: 150px;
+      background: url('/assets/earth.jpg');
+      border-radius: 50%;
+      background-size: 610px;
+      box-shadow: inset 8px 36px 80px 36px rgb(0, 0, 0, 0.6),
+        inset -6px 0 12px 4px rgba(255, 255, 255, 0.3);
+      animation-name: rotate;
+      animation-duration: 12s;
+      animation-iteration-count: infinite;
+      animation-timing-function: linear;
+      -webkit-animation-name: rotate;
+      -webkit-animation-duration: 12s;
+      -webkit-animation-iteration-count: infinite;
+      -webkit-animation-timing-function: linear;
+      margin-bottom: 20px;
+      @keyframes rotate {
+        from {
+          background-position: 0px 0px;
+        }
+        to {
+          background-position: 610px 0px;
+        }
+      }
+      @-webkit-keyframes rotate {
+        from {
+          background-position: 0px 0px;
+        }
+        to {
+          background-position: 610px 0px;
+        }
+      }
+    `}
+  ></div>
+);
+
 export const SignInPage: React.FC = () => {
   const signInForm = isLocal ? (
-    <LocalGateSignUpForm gate={gate as LocalGateStore} logo={<IconEarth size="100px" />} />
+    <LocalGateSignUpForm gate={gate as LocalGateStore} logo={<Logo />} />
   ) : (
     <GateSignUpForm
       gate={gate as GateStore}
-      logo={<IconEarth size="100px" />}
+      logo={<Logo />}
       mayHideEmailLogin={false}
       initShownEmailLogin={false}
     />
